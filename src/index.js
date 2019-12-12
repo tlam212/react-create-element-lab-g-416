6cb7d7b59592a87cbc959b449dde6f553ca6fe5d
Toni L <tlam212@gmail.com>
2019-12-12 19:09:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const title = ('h1', {}, 'An Awesome Person');
+const para = ('p', {}, 'Who is learning React');
+const ulList = ('ul', {className:"my-interests"}, [js, react, movies, ice cream]);
+const meInReact = ('div', {className:"me"}, [title, para, ulList]);
+
 ReactDOM.render(
   meInReact, 
   document.getElementById('root')
 );
 
 export default meInReact
-*/
